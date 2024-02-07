f62db7a4a60a748e101307a680a88f095e945475
Prashant1125 <145115819+Prashant1125@users.noreply.github.com>
2024-02-07 16:07:12 +0000

    Complete

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function addition(){
-  let num1 = document.getElementById("a");
-  let num2 = document.getElementById("b");
+  let num1 = parseInt(document.getElementById("a").value);
+  let num2 = parseInt(document.getElementById("b").value);
   let sum = 0 ;
-  for(let i = parseInt(num1.value) ; i <= parseInt(num2.value) ; i++){
-    sum += i ;
+   if(num1 < num2){
+  for(let i = num1 ; i <= num2 ; i++){
+  sum+=i;
+  }
+}
+else{
+    for(let i =num1 ; i >= num2 ; i++){
+    sum+=i;
+      }
   }
   let res = document.getElementById("res");
   res.innerHTML = sum ;
